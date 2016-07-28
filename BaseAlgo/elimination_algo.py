50c9327cab81b1273f0e7008653cb540e2987f19
Tushar Mathew <tusharmathew@gmail.com>
2016-07-27 20:48:31 -0500

    Working Algo with error rate calculations

--- BaseAlgo/elimination_algo.py
+++ BaseAlgo/elimination_algo.py
@@ -1,13 +1,18 @@
 import csv
 from math import pow,log
+from codecs import open
+import random
+
 
 #####################################
 # Initialization Variables
-mu = 1.00
-epsilon = 0.2
-anomalythreshold = 75.00
-eta = 0.2
-delta = 0.01
+mu = 0.4
+epsilon = 0.017            # for 21656 use 0.027, 269 = 0.17
+                            # fraction of epsilon that we can classify as anomaly (0.06-0.1)
+anomalythreshold = 0.53
+eta = 2    #0.2
+delta = 0.05
+# mu = (1.00+epsilon)*0.04
 #####################################
 
 ########################################################
@@ -24,19 +29,22 @@ delta = 0.01
 def tolerantelim(mu, eps, anothresh, rowlen, numrow, data):
     falsepos = []               # Matrix that will store the false positive count
     C = []                      # Matrix that will store the attributes to be returned
-    fin = []                    # Matrix to store the position numbers of attributes
+    fin = []                    # Matrix that contains the positions of the attributes
     for z in range(rowlen-1):
         falsepos.append(0)
         C.append(1)
 
     for row in data:
-        if float(row[rowlen]) < anothresh:
-            for i in range(1, rowlen):
-                if row[i] == '1':
+
+        if float(row[rowlen]) < anothresh:      # To single out the non-anomalies
+            for i in range(1, rowlen):          # In each row tagged as non-anomalous, find the atts that are positive
+                if row[i] == '1':               # and increment the false positive count of that attribute
                     falsepos[i-1] +=1
-            for i in range(len(C)):
-                if falsepos[i-1] > mu * eps * numrow:
-                    C[i-1] = 0
+            for i in range(len(C)):                         # Check if the false positive count of any of the attributes
+                if falsepos[i-1] > mu * eps * numrow:       # exceeds our threshold and if it does, then remove it from
+                    C[i-1] = 0                              # our final formula
+    # print falsepos
+    print  "the false pos bound is  : " + str(mu * eps * numrow)
     for pos in range(len(C)):
         if C[pos] == 1:
             fin.append(pos+1)
@@ -46,8 +54,10 @@ def tolerantelim(mu, eps, anothresh, rowlen, numrow, data):
 
 def create_matrix(filename, matrix = None):
     tmp=0
+    header = []
     if matrix is None:
         matrix = []
+
         tmp = 1
     exampleFile = open(filename)
     exampleReader = csv.reader(exampleFile)
@@ -56,49 +66,58 @@ def create_matrix(filename, matrix = None):
         if rowcount == 0:  # make sure that the header row is not appended to the matrix
             rowcount += 1
             rowlen = len(row)  # Get the length of the row to asses the number of attributes
+            if tmp == 1:
+                header = row
+
+            # print rowlen
+            # print rowcount
         else:
             if tmp == 1:
                 rowcount += 1
                 matrix.append(row)  # Make a 2D array of the boolean data and store it in the array matrix
             else:
-                matrix[rowcount - 1].append(row[1])  # Make a 2D array of the boolean data and store it in the array matrix
+
+                matrix[rowcount - 1].append(row[rowlen-1])  # Make a 2D array of the boolean data and store it in the array matrix
                 rowcount += 1
 
     exampleFile.close()
     rowcount -= 1   # to eliminate the header row that was counted
-    return matrix, rowcount, rowlen
+    return matrix, rowcount, rowlen, header
 
 
 
 def classifier_anocount( data, C):
     x = 0
     for row in data:
-        tmp = 1
-        for cnt in range(len(C)):
-            tmp = tmp and row[C[cnt]]
+        tmp = 0
+        for cnt in C:       # OR all the attributes in each row contained in the returned formula C
+            tmp = bool(tmp) or bool(row[cnt] == '1')
         if tmp:
             x += 1
+
     return x
 
 
+# Get the data as matrices
+matrix, rowcount, rowlen, labels= create_matrix('269/binary-269.csv')
+matrix, elim1, elim2 , elim3= create_matrix('269/PCA-269.csv', matrix)
 
-
-matrix, rowcount, rowlen = create_matrix('Synthetic_Data_Creation/Output/syntheticdata_attributes.csv')
-matrix , elim1, elim2 = create_matrix('Synthetic_Data_Creation/Output/syntheticdata_anomalyscore.csv', matrix)
-
+# Run the first iteration of tolerant elimination and set the iternation number
 thetest = tolerantelim(mu, epsilon, anomalythreshold, rowlen, rowcount, matrix)
-
-
 iternum = 1
+
+
 deltai = delta
-while ((classifier_anocount( matrix, thetest)/rowcount) < (mu/(1+eta))):
+
+
+while ((float(classifier_anocount(matrix, thetest))/float(rowcount)) < (mu/(1+eta))):
     var1 = (3.0*pow(1.0+eta, 2+iternum)*log((2.0*iternum*(iternum+1.0))/delta))/(pow(eta, 2))
     var2 = (3.0*log(2.0*(rowlen-1)/deltai))/(mu*epsilon*(1.0+eta)*pow(eta, 2))
     print "The iteration number is : " + str(iternum)
     print "Variable 1 is : " + str(var1)
     print "Variable 2 is : " + str(var2)
     print "mu : " + str(mu)
-    print "Quaifying condition is : " + str(classifier_anocount(matrix, thetest)/float(rowcount)) +\
+    print "Quaifying condition of while loop : " + str(classifier_anocount(matrix, thetest)/float(rowcount)) +\
           " is less than " + str((mu/(1+eta)))
 
     if rowcount > var1 and rowcount > var2:
@@ -108,15 +127,37 @@ while ((classifier_anocount( matrix, thetest)/rowcount) < (mu/(1+eta))):
         thetest = tolerantelim(mu, epsilon, anomalythreshold, rowlen, rowcount, matrix)
         print "The attributes remaining in the formula are : " + str(thetest)
     else:
-        print "An Optimal Classifier could not be found!!!!"
+        #if iternum == 1:
+        raise ValueError('An Optimal Classifier could not be found!!!!')
         break
 
+finalclassifier = []
+for count in thetest:
+    finalclassifier.append(labels[count])
 
-# print classifier_anocount( matrix, thetest)
-print "Last known classifier is : " + str(thetest)
+print "\n--------------A RESULT HAS BEEN FOUND!!!!!!---------------"
+print "Out of " + str(len(labels)-1) + " attributes, " + str(len(finalclassifier)) + " attriutes are in the final" \
+                                                                                     " classifier"
+print "Last known classifier is : " + str(finalclassifier)
 print "Algorithm went through " + str(iternum) + " iterations"
-print "The probability of strinking an image as anomaly is : " + str(mu)
+print "The probability of striking an image as anomaly is : " + str(mu)
 
+total_anomalies = 0.00
+total_false_anomalies = 0.00
+for row in matrix:
+    pca = len(row)-1
+    tmp = 0
+    for x in thetest:
+        tmp = bool(tmp) or bool(row[x] == '1')
+    if tmp:
+        total_anomalies+=1.00
+        # print row[pca]
+        if float(row[pca]) < 0.53:
+            total_false_anomalies+=1.00
+    elif (not tmp) and float(row[pca]) > 0.53:
+        print str(row[pca])
 
-
+print "The Error rate is : " + str(total_false_anomalies/total_anomalies)
+print total_false_anomalies
+print total_anomalies
 
